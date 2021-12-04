7e3eece3e58248f9b0ce2f2ca50c24f10f624ffa
Will Darkins <willdarkins@gmail.com>
2021-12-04 10:19:06 -0800

    more middleware added for mongodb and app.listen statement

--- server.js
+++ server.js
@@ -7,3 +7,16 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+//middleware telling mongoose which database we want to connect with
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-api-shell', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`😁👍 Connected on localhost:${PORT}`));
