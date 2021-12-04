7e720b7237a14bbe3149b25d075a7297755900e1
Will Darkins <willdarkins@gmail.com>
2021-12-04 12:45:08 -0800

    email and thoughts fields filled out, but still waiting to create thoughts model to draw reference

--- models/User.js
+++ models/User.js
@@ -2,19 +2,23 @@ const { Schema, model } = require('mongoose');
 
 const UserSchema = new Schema(
     {
-    username: {
-        type: String,
-        unique: true,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/]
-    },
-    
-    }
+        username: {
+            type: String,
+            unique: true,
+            required: [true, 'username is required'],
+            trim: true
+        },
+        email: {
+            type: String,
+            required: [true, 'email is required'],
+            unique: true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/]
+        },
+        thoughts: {
+            
+        }
+    })
 
-)
+    const User = model('User', UserSchema); 
+
+    module.exports = User;
