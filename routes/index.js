ba9ef95140ba9defbb3c57d814b6be8a70edcb66
Will Darkins <willdarkins@gmail.com>
2021-12-04 10:34:46 -0800

    set up index.js for routes to import api route statements to their endpoints

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+//this file imports all of the API routes to prefix their endpoint names and package them up
+const router = require('express').Router()
+const userRoutes = require('./user-routes');
+const thoughtRoutes = require('./thought-routes');
+
+//add prefix of ' /pizzas' to routes created in 'pizza-routes'
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
 
 app.use(require('./routes'));
 
@@ -19,4 +18,4 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-ap
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
 
-app.listen(PORT, () => console.log(`😁👍 Connected on localhost:${PORT}`));
+app.listen(PORT, () => console.log(`😁👍 Connected to ${PORT}, BRUH!!!`));
