6b62b43ab8b95eb801b2f7493f6500548d926260
Will Darkins <willdarkins@gmail.com>
2021-12-04 12:26:12 -0800

    imported schema from mongoose and filled out first username field

---
+++ models/User.js
@@ -0,0 +1,14 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    
+    }
+
+)
