431c699e8c7f67618eff30aa55b2b1c401d86de2
Will Darkins <willdarkins@gmail.com>
2021-12-04 10:15:30 -0800

    middleware added to server.js

--- server.js
+++ server.js
@@ -3,3 +3,7 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
