2214a6c568c170f26d49cb6dd024143d8cae985f
Will Darkins <willdarkins@gmail.com>
2021-12-04 21:14:54 -0800

    added addUser function to userController

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,5 +2,15 @@ const { User } = require('../models');
 const { Thought } = require('../models');
 
 const userController = {
-    
+    addUser({body}, res) {
+        User.create(body)
+        .then(dbUser => {
+            res.json(dbUser);
+        })
+        .catch(err => {
+            res.json(err)
+        })
+    }
 }
+
+module.exports = userController;
