54f9458bb425a8990b096fae16084b251d82c800
Will Darkins <willdarkins@gmail.com>
2021-12-05 08:27:08 -0800

    tested routes for posting comment with username attached... it works!

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
+const {
+    addThought
+} = require('../../controllers/thought-controller')
 
 router
-.route('/')
+.route('/:userId')
+.post(addThought)
+
 module.exports = router
