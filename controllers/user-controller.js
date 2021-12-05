6a01236fb590dd88b978f5ce449409a21818ac9c
Will Darkins <willdarkins@gmail.com>
2021-12-05 00:46:59 -0800

    added getUserById function to user-controller.js

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -13,7 +13,24 @@ const userController = {
         .then(dbUser => {
             res.json(dbUser);
         })
-        .catch(err => res.status(400).json(err))
-    }
+        .catch(err => {
+            res.status(400).json(err)
+        })
+    },
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+        .then(dbUser => {
+            if(!dbUser){
+                res.status(400).json({message: 'There is no user with that ID!'});
+                return
+            }
+            res.json(dbUser);
+        })
+        .catch(err=> {
+            console.log(err);
+            res.status(400).json(err)
+        })
+    },
+    
 }   
 module.exports = userController;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,12 +1,17 @@
 const router = require('express').Router();
 const {
     addUser,
-    getAllUsers
+    getAllUsers,
+    getUserById
 } = require('../../controllers/user-controller')
 
 router
 .route('/')
 .post(addUser)
-.get(getAllUsers)
+.get(getAllUsers);
+
+router
+.route('/:id')
+.get(getUserById)
 
 module.exports = router
