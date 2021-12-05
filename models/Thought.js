18a20b2c81b63815b8e87310e6076ff50cb33c90
Will Darkins <willdarkins@gmail.com>
2021-12-04 18:22:35 -0800

    added reactionCount virtual to Thought schema on Thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -11,12 +11,31 @@ const ThoughtSchema = new Schema(
         createdAt: {
             type: Date,
             default: Date.now,
+        },
+        username: {
+            type: String,
+            required: [true, 'A username is required']
+        },
+        reactions: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Reaction'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
         }
     }
 )
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length
+})
 const ReactionSchema = new Schema(
 
-    )
+)
 
 const Thought = model('Thought', ThoughtSchema);
 const Reaction = model('Reaction', ReactionSchema);
