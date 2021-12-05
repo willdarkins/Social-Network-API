b7cb90004665109296c625ead051483ee138b237
Will Darkins <willdarkins@gmail.com>
2021-12-04 21:19:15 -0800

    created index page for root routes directory

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const apiRoutes = require('./api')
+
+// add prefix of `/api` to all of the api routes imported from the `api` directory
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+  res.status(404).send('<h1>You done fucked up...</h1>');
+});
+
+module.exports = router;
